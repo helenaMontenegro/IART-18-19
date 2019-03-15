d4ddb1beeb49031b5dc8f64ac8f069c77e4009c8
Helena Montenegro <helena.montenegro.98@gmail.com>
2019-03-15 21:10:10 +0000

    improved bfs to not check the same board twice

--- Unblock Me/src/BFS.java
+++ Unblock Me/src/BFS.java
@@ -2,15 +2,28 @@ import java.util.ArrayList;
 
 public class BFS {
 	ArrayList<Board> boards_to_expand;
+	ArrayList<Board> boards_explored;
 	
 	BFS(Board initial_board) {
 		boards_to_expand = new ArrayList<>();
+		boards_explored = new ArrayList<>();
 		boards_to_expand.add(initial_board);
 	}
 	
 	public Board run() {
 		while(!boards_to_expand.isEmpty()) {
+			boolean already_explored = false;
 			Board parent = boards_to_expand.get(0);
+			for(int n = 0; n < boards_explored.size(); n++) {
+				if(parent.compare_board(boards_explored.get(n))) {
+					already_explored = true;
+					continue;
+				}
+			}
+			if(already_explored) {
+				boards_to_expand.remove(0);
+				continue;
+			}
 			ArrayList<Board> successors = parent.generate_successors();
 			for(int i = 0; i < successors.size(); i++) {
 				if(successors.get(i).is_final()) {
@@ -18,6 +31,7 @@ public class BFS {
 				}
 			}
 			boards_to_expand.addAll(successors);
+			boards_explored.add(boards_to_expand.get(0));
 			boards_to_expand.remove(0);
 		}
 		return null;
