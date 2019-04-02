121a5eb101b942bfedb5b5883e39bed4535b7eee
up201603526@FEUPsig.fe.up.pt <up201603526@FEUPsig.fe.up.pt>
2019-04-02 21:18:13 +0100

    Added documentation and refactoring

--- Unblock Me/src/gui/Grid.java
+++ Unblock Me/src/gui/Grid.java
@@ -11,67 +11,63 @@ import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-    public class Grid extends JPanel {
-    	private int[][] ini_board = null;
-        private List<Point> fillCells;
+public class Grid extends JPanel {
+	private int[][] ini_board = null;
+	private List<Point> fillCells;
 
-        public Grid() {
-            fillCells = new ArrayList<>(25);
-        }
-        
-        public void setBoard(int[][] board) {
-        	this.ini_board = board;
-        	for(int i = 0; i < ini_board.length; i++) {
-        		for(int j = 0; j < ini_board[i].length;j++) {
-        			fillCell(i, j);
-        		}
-        	}
-     
-        }
+	public Grid() {
+		fillCells = new ArrayList<>(25);
+	}
 
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            for (Point fillCell : fillCells) {
-                int cellX = 15 + (fillCell.x * 15);
-                int cellY = 15 + (fillCell.y * 15);
-                if(ini_board != null) {
-                	if(ini_board[fillCell.y][fillCell.x] == 0) {
-                		g.setColor(Color.WHITE);
-                	}
-                	else if(ini_board[fillCell.y][fillCell.x] == 1) {
-                		g.setColor(Color.BLUE);
-                	} else if (ini_board[fillCell.y][fillCell.x] % 2 == 0 && ini_board[fillCell.y][fillCell.x] != -2){
-                		g.setColor(Color.RED);
-                	}
-                	else if (ini_board[fillCell.y][fillCell.x] == -2){
-                		g.setColor(Color.DARK_GRAY);
-                	}
-                	else if (ini_board[fillCell.y][fillCell.x] == -1){
-                		g.setColor(Color.WHITE);
-                	}
-                	else {
-                		g.setColor(Color.GREEN);
-                	}
-                }
-                
-                g.fillRect(cellX, cellY, 15, 15);
-            }
-            g.setColor(Color.BLACK);
-            g.drawRect(15, 15, 195, 150);
+	public void setBoard(int[][] board) {
+		this.ini_board = board;
+		for (int i = 0; i < ini_board.length; i++) {
+			for (int j = 0; j < ini_board[i].length; j++) {
+				fillCell(i, j);
+			}
+		}
 
-            for (int i = 15; i <= 200; i += 15) {
-                g.drawLine(i, 15, i, 165);
-            }
+	}
 
-            for (int i = 15; i <= 150; i += 15) {
-                g.drawLine(15, i, 210, i);
-            }
-        }
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		for (Point fillCell : fillCells) {
+			int cellX = 15 + (fillCell.x * 15);
+			int cellY = 15 + (fillCell.y * 15);
+			if (ini_board != null) {
+				if (ini_board[fillCell.y][fillCell.x] == 0) {
+					g.setColor(Color.WHITE);
+				} else if (ini_board[fillCell.y][fillCell.x] == 1) {
+					g.setColor(Color.BLUE);
+				} else if (ini_board[fillCell.y][fillCell.x] % 2 == 0 && ini_board[fillCell.y][fillCell.x] != -2) {
+					g.setColor(Color.RED);
+				} else if (ini_board[fillCell.y][fillCell.x] == -2) {
+					g.setColor(Color.DARK_GRAY);
+				} else if (ini_board[fillCell.y][fillCell.x] == -1) {
+					g.setColor(Color.WHITE);
+				} else {
+					g.setColor(Color.GREEN);
+				}
+			}
 
-        public void fillCell(int x, int y) {
-            fillCells.add(new Point(x, y));
-            repaint();
-        }
+			g.fillRect(cellX, cellY, 15, 15);
+		}
+		g.setColor(Color.BLACK);
+		g.drawRect(15, 15, 195, 150);
 
-    }
+		for (int i = 15; i <= 200; i += 15) {
+			g.drawLine(i, 15, i, 165);
+		}
+
+		for (int i = 15; i <= 150; i += 15) {
+			g.drawLine(15, i, 210, i);
+		}
+	}
+
+	public void fillCell(int x, int y) {
+		fillCells.add(new Point(x, y));
+		repaint();
+	}
+
+}
