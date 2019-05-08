f63a4170a6bd6156020e3f7564050888c0a10293
Helena Montenegro <helena.montenegro.98@gmail.com>
2019-05-08 17:11:37 +0100

    separated pc vs pc in one class

--- Mancala/src/Main.java
+++ Mancala/src/Main.java
@@ -2,14 +2,30 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Board board = new Board(new int[][] {{4,4,4,4,4,4},{4,4,4,4,4,4}}, 7, 1, "MAX", 1);
+		Main m = new Main();
+		m.pcVSpc(2, 1);
+	}
+	
+	public int get_depth(int difficulty) {
+		if(difficulty == 1)
+			return 3;
+		if(difficulty == 2)
+			return 5;
+		return 7;
+	}
+	
+	public void pcVSpc(int pc1_difficulty, int pc2_difficulty) {
+		int depth1 = this.get_depth(pc1_difficulty);
+		int depth2 = this.get_depth(pc2_difficulty);
+		
+		Board board = new Board(new int[][] {{4,4,4,4,4,4},{4,4,4,4,4,4}}, depth1, 1, "MAX", 1);
 		board.print();
-		MinimaxSearch m = new MinimaxSearch(board);
+		MinimaxSearch minimax = new MinimaxSearch(board, depth1, depth2);
 		while(!board.is_final()) {
-			board = m.run();
-			m.set_board(board);
+			board = minimax.run();
+			minimax.set_board(board);
 			board.print();
 		}
-		board.print_result();
+		board.print_result();				
 	}
 }
--- Mancala/src/MinimaxSearch.java
+++ Mancala/src/MinimaxSearch.java
@@ -1,16 +1,21 @@
 
 public class MinimaxSearch {
-	private int depth;
+	private int depth1;
+	private int depth2;
 	private Board board;
 	
-	public MinimaxSearch(Board board) {
+	public MinimaxSearch(Board board, int depth1, int depth2) {
 		this.board = board;
-		this.depth = this.board.get_num_levels();
+		this.depth1 = depth1;
+		this.depth2 = depth2;
 	}
 	
 	public void set_board(Board board) {
 		this.board = board;
-		this.board.set_num_levels(this.depth);
+		if(this.board.get_players_turn() == 1)
+			this.board.set_num_levels(this.depth1);
+		else
+			this.board.set_num_levels(this.depth2);
 		this.board.set_successors(null);
 		this.board.set_keyword("MAX");
 		this.board.set_value(-1);
@@ -21,7 +26,6 @@ public class MinimaxSearch {
 		Board next_board;
 		this.board.generate_successors();
 		next_board = this.board.get_best_board();
-		
 		return next_board;
 	}
 }
