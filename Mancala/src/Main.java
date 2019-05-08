e1f02b90e67916068eb7b4e9c934a705a22aa5ab
SmilingOwl <julianamcm05@gmail.com>
2019-05-08 09:58:59 +0100

    added evaluation function

--- Mancala/src/Main.java
+++ Mancala/src/Main.java
@@ -1,14 +1,16 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		Board board = new Board(new int[][] {{4,4,4,4,4,4},{4,4,4,4,4,4}}, 1, 1, "MAX");
-		board.print();
-		Board b2 = board.movement(0);
-		b2.print();
-		Board b_final = new Board(new int[][] {{0,0,0,0,0,0},{0,0,1,2,0,0}}, 1, 1, "MAX");
-		if(b_final.is_final()) {
-			b_final.print_result();
-		}
-	}
+    public static void main(String[] args) {
+        Board board = new Board(new int[][]{{4, 4, 4, 4, 4, 4}, {4, 4, 4, 4, 4, 4}}, 1, 1, "MAX");
+        Board b_final = new Board(new int[][]{{0, 0, 0, 0, 0, 0}, {0, 0, 1, 2, 0, 0}}, 1, 1, "MAX");
+        board.print();
+
+        Board b2 = board.movement(0);
+        b2.print();
+
+        if (b_final.is_final()) {
+            b_final.print_result();
+        }
+    }
 }
