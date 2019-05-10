1fe9e598f41ce453cb3b821ea799b646be05db11
SmilingOwl <julianamcm05@gmail.com>
2019-05-10 13:07:57 +0100

    added main menu and pc Vs pc menu

--- Mancala/src/Main.java
+++ Mancala/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
 
@@ -28,5 +29,113 @@ public class Main {
 		}
 		board.set_final(); //updates mancala with the end of game
 		board.print_result();				
+	}
+
+	/**
+	 * Function that shows the initial menu of the program
+	 * @param board - initial board.
+	 */
+	public static void mainMenu(Board board) {
+		System.out.println("---------------------------------------");
+		System.out.println("|   __  __                   _         |");
+		System.out.println("|  |  \\/  |__ _ _ _  __ __ _| |__ _    |");
+		System.out.println("|  | |\\/| / _` | ' \\/ _/ _` | / _` |   |");
+		System.out.println("|  |_|  |_\\__,_|_||_\\__\\__,_|_\\__,_|   |");
+		System.out.println("|                                      |");
+		System.out.println("|           1-Human Vs Human           |");
+		System.out.println("|           2-PC Vs PC                 |");
+		System.out.println("|           3-Human Vs Pc              |");
+		System.out.println("|                                      |");
+		System.out.println("---------------------------------------");
+
+
+		int option = getOption(1, 3);
+
+		switch (option) {
+			case 1:
+				humanMenu(board);
+				break;
+			case 2:
+				pcMenu(board);
+				break;
+			case 3:
+				humanPcMenu(board);
+			default:
+				break;
+		}
+
+
+	}
+
+public static void pcMenu(Board board){
+
+	Main m = new Main();
+
+	System.out.println("-------------------------------------------------------");
+	System.out.println("|                                                      |");
+	System.out.println("|    Please, choose the difficulty of the first PC:    |");
+	System.out.println("|      1-Beginner                                      |");
+	System.out.println("|      2-Medium                                        |");
+	System.out.println("|      3-Professional                                  |");
+	System.out.println("|                                                      |");
+	System.out.println("-------------------------------------------------------");
+
+	int option = getOption(1, 3);
+
+	System.out.println("-------------------------------------------------------");
+	System.out.println("|                                                      |");
+	System.out.println("|    Please, choose the difficulty of the second PC:   |");
+	System.out.println("|      1-Beginner                                      |");
+	System.out.println("|      2-Medium                                        |");
+	System.out.println("|      3-Professional                                  |");
+	System.out.println("|                                                      |");
+	System.out.println("-------------------------------------------------------");
+
+	int option2 = getOption(1, 3);
+
+	m.pcVSpc(option, option2);
+}
+
+public static void humanMenu(Board board){
+
+
+}
+
+public static void humanPcMenu(Board board){
+
+	System.out.println("-------------------------------------------------------");
+	System.out.println("|                                                      |");
+	System.out.println("|    Please, choose the difficulty of the PC:          |");
+	System.out.println("|      1-Beginner                                      |");
+	System.out.println("|      2-Medium                                        |");
+	System.out.println("|      3-Professional                                  |");
+	System.out.println("|                                                      |");
+	System.out.println("-------------------------------------------------------");
+
+	int option = getOption(1, 3);
+
+}
+	/**
+	 * Function that receives an input from the user between min and max and returns it.
+	 * @param min - minimum value
+	 * @param max - maximum value
+	 * @return user input
+	 */
+	private static int getOption(int min, int max) {
+		int option = 0;
+		do {
+			try {
+				System.out.println("Option:");
+				Scanner sc = new Scanner  (System.in);
+				option = sc.nextInt();
+			} catch (Exception e) {
+				System.out.println("Invalid Input");
+				continue;
+			}
+			if(option < min || option > max){
+				System.out.println("Invalid Option");
+			}
+		} while (option < min || option > max);
+		return option;
 	}
 }
